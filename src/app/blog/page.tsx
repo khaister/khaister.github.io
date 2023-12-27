53732bac56b66e97e6335bf0950498ae69411d63
Khai Nguyen <1192669+khaister@users.noreply.github.com>
2023-12-26 22:00:26 -0800

    Don't export getPosts

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -20,7 +20,7 @@ export default async function Blog() {
   )
 }
 
-export async function getPosts(): Promise<any> {
+async function getPosts(): Promise<any> {
   const promise = new Promise((resolve, reject) => {
     resolve([
       {
