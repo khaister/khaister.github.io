c2bcf892267a42fbabdd251df4f1f9f13396f5a8
Khai Nguyen <1192669+khaister@users.noreply.github.com>
2023-12-27 00:35:41 -0800

    Fix screen height (#18)

--- src/app/meta/page.tsx
+++ src/app/meta/page.tsx
@@ -1,6 +1,6 @@
 export default function Meta() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between p-24">
       <div>
         Meta coming soon!
       </div>
