582a2a62fd74e3560fb4f6e124ffd33d1a72aeef
Khai Nguyen <1192669+khaister@users.noreply.github.com>
2024-01-19 22:08:31 -0800

    build with astro (#21)

--- content/blog/reading.md
+++
@@ -1,5 +0,0 @@
----
-title: Reading List
-date: "2023-11-12"
-slug: reading-list
----
--- src/app/meta/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Meta() {
-  return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <div>
-        Meta coming soon!
-      </div>
-    </main>
-  )
-}
