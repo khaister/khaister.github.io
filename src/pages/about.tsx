bf6ce703b28c93d01a79e18bbc754dabf3232528
Khai Nguyen <1192669+khaister@users.noreply.github.com>
2024-02-20 21:45:05 -0800

    Add stub about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,10 @@
+import Layout from '@theme/Layout';
+
+export default function About(): JSX.Element {
+  return (
+    <Layout
+      title={`About`}>
+      <div>a lil' bit about me</div>
+    </Layout>
+  )
+}
