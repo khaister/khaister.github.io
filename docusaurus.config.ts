ec26dfed378a01fc11d6cbac8ef1765cddf208b3
Khai Nguyen <1192669+khaister@users.noreply.github.com>
2024-02-25 01:07:22 -0800

    Use icon instead of text for github link

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -72,9 +72,10 @@ const config: Config = {
         // { to: '/blog', label: 'Blog', position: 'left' },
         // { to: '/about', label: 'About', position: 'left' },
         {
-          href: 'https://github.com/khaister',
-          label: 'GitHub',
+          to: 'https://github.com/khaister',
           position: 'right',
+          label: ' ',
+          className: 'header-github-link group',
         },
       ],
     },
