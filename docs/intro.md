d2ebb2be65d15c93cbda7fda52699be391a139d4
Khai Nguyen <1192669+khaister@users.noreply.github.com>
2024-02-24 18:41:35 -0800

    Blog with docusaurus (#49)

---
+++ docs/intro.md
@@ -0,0 +1,6 @@
+---
+title: Intro
+sidebar_position: 1
+---
+
+A selection of documentations, technical or otherwise.
---
+++ docusaurus.config.ts
@@ -0,0 +1,94 @@
+import type * as Preset from '@docusaurus/preset-classic';
+import type { Config } from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+
+const config: Config = {
+  title: 'Khai',
+  tagline: '',
+  favicon: '/favicon.ico',
+
+  url: 'https://khaister.github.io',
+  baseUrl: '/',
+
+  organizationName: 'khaister',
+  projectName: 'khaister.github.io',
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+        },
+        blog: {
+          showReadingTime: true,
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // TODO: Replace with your project's social card
+    // image: 'img/docusaurus-social-card.jpg',
+    headTags: [
+      {
+        tagName: 'link',
+        attributes: {
+          href: 'https://cdn.jsdelivr.net/gh/eliyantosarage/font-awesome-pro@main/fontawesome-pro-6.5.1-web/css/all.min.css',
+          rel: 'stylesheet',
+        },
+      },
+    ],
+    colorMode: {
+      defaultMode: 'light',
+      respectPrefersColorScheme: true,
+    },
+    navbar: {
+      title: 'Khai',
+      // TODO: Replace with your project's logo
+      // logo: {
+      //   alt: 'Khai Logo',
+      //   src: 'img/logo.svg',
+      // },
+      items: [
+        // TODO: Add docs, blog, and about links
+        // {
+        //   type: 'docSidebar',
+        //   sidebarId: 'wikiSidebar',
+        //   position: 'left',
+        //   label: 'Docs',
+        // },
+        // { to: '/blog', label: 'Blog', position: 'left' },
+        // { to: '/about', label: 'About', position: 'left' },
+        {
+          href: 'https://github.com/khaister',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'light',
+      links: [],
+      copyright: `Copyright © ${new Date().getFullYear()}. Built with Docusaurus.`,
+    },
+    prism: {
+      additionalLanguages: [],
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
