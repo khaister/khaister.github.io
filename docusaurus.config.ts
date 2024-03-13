5ecdab7eea34db1d43194d66891a353e8851d11c
Khai <1192669+khaister@users.noreply.github.com>
2024-03-12 23:51:15 -0700

    Add font-awesome assets (#54)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,6 +6,7 @@ const config: Config = {
   title: 'Khai',
   tagline: '',
   favicon: '/favicon.ico',
+  staticDirectories: ['public', 'static'],
 
   url: 'https://khaister.github.io',
   baseUrl: '/',
@@ -20,6 +21,38 @@ const config: Config = {
     defaultLocale: 'en',
     locales: ['en'],
   },
+
+  headTags: [
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'stylesheet',
+        href: 'https://cdn.jsdelivr.net/gh/eliyantosarage/font-awesome-pro@main/fontawesome-pro-6.5.1-web/css/all.min.css',
+      }
+    },
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'preconnect',
+        href: 'https://fonts.googleapis.com',
+      }
+    },
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+        crossorigin: 'anonymous',
+      }
+    },
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=IBM+Plex+Sans+Condensed:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=IBM+Plex+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&family=IBM+Plex+Serif:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap',
+      }
+    }
+  ],
 
   presets: [
     [
@@ -41,15 +74,6 @@ const config: Config = {
   themeConfig: {
     // TODO: Replace with your project's social card
     // image: 'img/docusaurus-social-card.jpg',
-    headTags: [
-      {
-        tagName: 'link',
-        attributes: {
-          href: 'https://cdn.jsdelivr.net/gh/eliyantosarage/font-awesome-pro@main/fontawesome-pro-6.5.1-web/css/all.min.css',
-          rel: 'stylesheet',
-        },
-      },
-    ],
     colorMode: {
       defaultMode: 'light',
       respectPrefersColorScheme: true,
